1c332228988b7fa7d9ae359df1e901afff0a433e
lizhikui <lzkmm@sina.com>
2023-11-06 01:36:00 +0800

    压缩工具集成到代码里

---
+++ utils/pngquant/pngquant.go
@@ -0,0 +1,6 @@
+package pngquant
+
+import _ "embed"
+
+//go:embed pngquant.exe
+var PngquantBin []byte
--- utils/utils.go
+++ utils/utils.go
@@ -7,6 +7,8 @@ import (
 	_ "image/gif"
 	_ "image/jpeg"
 	"image/png"
+	"imgbed/utils/pngquant"
+	"log"
 	"os"
 	"os/exec"
 	"path"
@@ -40,6 +42,43 @@ func AllCompression(ss []string) error {
 	return nil
 }
 
+var pngquantPath = ""
+
+func init() {
+
+	gopath := os.Getenv("GOPATH")
+	if len(gopath) <= 0 {
+		log.Fatal("环境变量不存在")
+	}
+
+	pngquantPath = gopath + "/bin/" + "pngquant"
+
+	_, err := os.Stat(pngquantPath)
+	if err == nil {
+		//fmt.Println("文件已经存在")
+		return
+	}
+
+	if err != nil {
+		if os.IsNotExist(err) {
+			//fmt.Println("文件不存在")
+		} else {
+			log.Fatal("文件判断失败", err)
+		}
+	}
+
+	pngquantExe, err := os.OpenFile(pngquantPath, os.O_CREATE, 0666)
+	if err != nil {
+		log.Fatal("压缩工具复制失败", err)
+	}
+
+	defer pngquantExe.Close()
+	_, err = pngquantExe.Write(pngquant.PngquantBin)
+	if err != nil {
+		log.Fatal("压缩工具复制失败", err)
+	}
+}
+
 func ImgCompression(s string, d string) error {
 	imgFile, err := os.Open(s)
 	if err != nil {
@@ -55,7 +94,7 @@ func ImgCompression(s string, d string) error {
 	}
 
 	if format == "png" {
-		cmd := exec.Command("/Users/lizhikui/go/bin/pngquant", "--force", "--skip-if-larger", s, "-o", d)
+		cmd := exec.Command(pngquantPath, "--force", "--skip-if-larger", s, "-o", d)
 		err := cmd.Start()
 		if err != nil {
 			return err
